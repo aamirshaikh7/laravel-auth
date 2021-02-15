212bf9d659cd899716dd3dbd5fa0985bf2b98749
Aamir shaikh <aamirshaikh3232@gmail.com>
2021-02-15 19:02:39 +0530

    displaying/checking authenticated user

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,6 +45,16 @@
                     </svg>
                 </div>
 
+                <div>
+                    @auth
+                        <p>Welcome, {{ Auth::user()->name }}</p>
+                    @endauth
+
+                    @guest
+                        <p>Please Login</p>
+                    @endguest
+                </div>
+
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                     <div class="grid grid-cols-1 md:grid-cols-2">
                         <div class="p-6">
